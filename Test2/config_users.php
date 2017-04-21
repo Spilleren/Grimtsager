f5f4a18cf58a16cee5e5e14c96a9ffeb30e6bfd5
Tummas Jóhan Sigvardsen <tsigva16@student.aau.dk>
2017-04-21 13:40:34 +0200

    Uploaded
    config_users.php

---
+++ Test2/config_users.php
@@ -0,0 +1,10 @@
+<?php
+
+	$hostnamedb = 'localhost';
+	$usernamedb = 'root';
+	$passworddb = '';
+	$databasedb = 'turmz_spasser_users';
+
+	$mysqli = mysqli_connect($hostnamedb, $usernamedb, $passworddb, $databasedb);
+
+?>
