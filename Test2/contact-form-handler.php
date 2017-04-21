bd01bb74cb75fb34a129ee606f584f98ea63f10d
Tummas Jóhan Sigvardsen <tsigva16@student.aau.dk>
2017-04-21 13:07:05 +0200

    Contact virker
    Contact virker

---
+++ Test2/contact-form-handler.php
@@ -0,0 +1,48 @@
+<?php 
+$errors = '';
+$myemail = 'turmz92@gmail.com';
+if(empty($_POST['name'])  || 
+   empty($_POST['email'])) 
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+
+if (!preg_match("/^[0-9]*$/", $email_address))
+	
+{
+    $errors .= "\n Error: Invalid phonenumber";
+}
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address"; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
